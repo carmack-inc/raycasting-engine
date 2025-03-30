fc54a15ab649f922b0dbd31964dbfb156ea7fbcc
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2025-03-30 19:47:25 -0300

    chore: update the header height

--- src/app/engine/layout.tsx
+++ src/app/engine/layout.tsx
@@ -19,7 +19,7 @@ export default function EngineLayout({ children }: EngineLayoutProps) {
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <header className="flex h-12 shrink-0 items-center gap-2 transition-[width] ease-linear border-b">
             <div className="flex items-center gap-2 px-4 w-full">
               <SidebarTrigger className="-ml-1" />
               <div className="ml-auto flex items-center gap-x-2">
@@ -28,7 +28,7 @@ export default function EngineLayout({ children }: EngineLayoutProps) {
               </div>
             </div>
           </header>
-          <div className="px-4">{children}</div>
+          <div className="p-4">{children}</div>
         </SidebarInset>
       </SidebarProvider>
     </Tabs>

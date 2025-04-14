af437250d6131d85f87aa71c928e6738a73ce4cf
ryuzera <ryugfx@gmail.com>
2025-04-14 09:05:56 -0300

    feat: fix coin icon, change game size and add under bar to the sidebar items

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -99,7 +99,7 @@ const groups: Group[] = [
         icon: ArrowUpRightIcon,
       },
       {
-        title: "CoinsIcon",
+        title: "Coins",
         value: "coins",
         icon: CoinsIcon,
       },
@@ -135,6 +135,7 @@ export function NavMain() {
       {groups.map((group, idx) => (
         <SidebarGroup key={idx}>
           {group.label && <SidebarGroupLabel>{group.label}</SidebarGroupLabel>}
+          <hr />
           <SidebarMenu>
             {group.items.map((item) => (
               <SidebarMenuItem key={item.title}>

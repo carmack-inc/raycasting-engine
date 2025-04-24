e8979c2484d880f43eca1e069f25ac58b50465da
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2025-04-24 13:42:52 -0300

    refactor: split header into a component

---
+++ src/app/engine/header.tsx
@@ -0,0 +1,17 @@
+import { GameDialog } from "@/components/game-dialog";
+import { ModeToggle } from "@/components/mode-toggle";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
+export function Header() {
+  return (
+    <header className="flex h-[--navbar-height] shrink-0 items-center gap-2 transition-[width] ease-linear border-b">
+      <div className="flex items-center gap-2 px-4 w-full">
+        <SidebarTrigger className="-ml-1" />
+        <div className="ml-auto flex items-center gap-x-2">
+          <ModeToggle />
+          <GameDialog />
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/app/engine/layout.tsx
+++ src/app/engine/layout.tsx
@@ -1,12 +1,7 @@
-import React from 'react'
-import { AppSidebar } from "@/components/app-sidebar"
-import { GameDialog } from "@/components/game-dialog"
-import { ModeToggle } from "@/components/mode-toggle"
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar"
+import { Header } from "@/app/engine/header";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import React from 'react';
 
 type EngineLayoutProps = Readonly<{
   children: React.ReactNode
@@ -17,15 +12,7 @@ export default function EngineLayout({ children }: EngineLayoutProps) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset className="min-w-0 [--navbar-height:theme(spacing.12)]">
-        <header className="flex h-[--navbar-height] shrink-0 items-center gap-2 transition-[width] ease-linear border-b">
-          <div className="flex items-center gap-2 px-4 w-full">
-            <SidebarTrigger className="-ml-1" />
-            <div className="ml-auto flex items-center gap-x-2">
-              <ModeToggle />
-              <GameDialog />
-            </div>
-          </div>
-        </header>
+        <Header />
         <div>{children}</div>
       </SidebarInset>
     </SidebarProvider>
